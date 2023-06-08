d30bb130af86fb7c445590e351a334de0dd2ec4a
cheolwo <powerqkrcjfd@gmail.com>
2023-06-08 16:52:01 +0900

    .

---
+++ FullFillMentSoulution/NaverCommon/네이버길찾기API.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NaverCommon.Diriection
+{
+    public class Summary
+    {
+        public class Location
+        {
+            public List<double> location { get; set; }
+        }
+
+        public Location start { get; set; }
+        public class Goal
+        {
+            public List<double> location { get; set; }
+            public int dir { get; set; }
+        }
+        public Goal goal { get; set; }
+        public int distance { get; set; }
+        public int duration { get; set; }
+        public List<List<double>> bbox { get; set; }
+        public int tollFare { get; set; }
+        public int taxiFare { get; set; }
+        public int fuelPrice { get; set; }
+    }
+
+    public class Section
+    {
+        public int pointIndex { get; set; }
+        public int pointCount { get; set; }
+        public int distance { get; set; }
+        public string name { get; set; }
+        public int congestion { get; set; }
+        public int speed { get; set; }
+    }
+
+    public class Guide
+    {
+        public int pointIndex { get; set; }
+        public int type { get; set; }
+        public string instructions { get; set; }
+        public int distance { get; set; }
+        public int duration { get; set; }
+    }
+
+    public class Trafast
+    {
+        public Summary summary { get; set; }
+        public List<List<double>> path { get; set; }
+        public List<Section> section { get; set; }
+        public List<Guide> guide { get; set; }
+    }
+
+    public class Route
+    {
+        public List<Trafast> trafast { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+        public string currentDateTime { get; set; }
+        public Route route { get; set; }
+    }
+    public class 네이버길찾기ApiClient
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+
+        public 네이버길찾기ApiClient(string clientId, string clientSecret)
+        {
+            _httpClient = new HttpClient();
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+        public async Task<RootObject> GetDrivingDirectionsAsync(string start, string goal, string option = "")
+        {
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+            _httpClient.DefaultRequestHeaders.Add("X-NCP-APIGW-API-KEY-ID", _clientId);
+            _httpClient.DefaultRequestHeaders.Add("X-NCP-APIGW-API-KEY", _clientSecret);
+
+            string url = $"https://naveropenapi.apigw.ntruss.com/map-direction/v1/driving?start={Uri.EscapeDataString(start)}&goal={Uri.EscapeDataString(goal)}";
+
+            if (!string.IsNullOrEmpty(option))
+            {
+                url += $"&option={Uri.EscapeDataString(option)}";
+            }
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(responseBody);
+
+            if (response.IsSuccessStatusCode)
+            {
+                RootObject result = JsonConvert.DeserializeObject<RootObject>(responseBody);
+                return result;
+            }
+            else
+            {
+                throw new Exception($"Request failed with status code {response.StatusCode}. Error message: {responseBody}");
+            }
+        }
+    }
+}
--- FullFillMentSoulution/TestApplication/Program.cs
+++ FullFillMentSoulution/TestApplication/Program.cs
@@ -1,106 +1,29 @@
-﻿using AutoMapper;
-using KoreaCommon.Model;
-using Newtonsoft.Json;
-using System;
+﻿using NaverCommon;
+using NaverCommon.Diriection;
 
 namespace ConsoleApp
 {
-    public class AddressElement
-    {
-        public List<string> types { get; set; }
-        public string longName { get; set; }
-        public string shortName { get; set; }
-        public string code { get; set; }
-    }
-
-    public class Address
-    {
-        public string roadAddress { get; set; }
-        public string jibunAddress { get; set; }
-        public string englishAddress { get; set; }
-        public List<AddressElement> addressElements { get; set; }
-        public string x { get; set; }
-        public string y { get; set; }
-        public double distance { get; set; }
-    }
-
-    public class Meta
-    {
-        public int totalCount { get; set; }
-        public int page { get; set; }
-        public int count { get; set; }
-    }
-
-    public class RootObject
-    {
-        public string status { get; set; }
-        public Meta meta { get; set; }
-        public List<Address> addresses { get; set; }
-        public string errorMessage { get; set; }
-    }
-
-    public class 네이버좌표변환ApiClient
-    {
-        private readonly HttpClient _httpClient;
-        private readonly string _clientId;
-        private readonly string _clientSecret;
-
-        public 네이버좌표변환ApiClient(string clientId, string clientSecret)
-        {
-            _httpClient = new HttpClient();
-            _clientId = clientId;
-            _clientSecret = clientSecret;
-        }
-
-        public async Task<RootObject> GetGeocodeAsync(string address, string coordinate = "", string filter = "", string language = "kor", int page = 1, int count = 10)
-        {
-            _httpClient.DefaultRequestHeaders.Clear();
-            _httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
-            _httpClient.DefaultRequestHeaders.Add("X-NCP-APIGW-API-KEY-ID", _clientId);
-            _httpClient.DefaultRequestHeaders.Add("X-NCP-APIGW-API-KEY", _clientSecret);
-
-            string url = $"https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query={Uri.EscapeDataString(address)}";
-
-            if (!string.IsNullOrEmpty(coordinate))
-            {
-                url += $"&coordinate={coordinate}";
-            }
-
-            if (!string.IsNullOrEmpty(filter))
-            {
-                url += $"&filter={Uri.EscapeDataString(filter)}";
-            }
-
-            url += $"&language={language}&page={page}&count={count}";
-
-            HttpResponseMessage response = await _httpClient.GetAsync(url);
-
-            string responseBody = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(responseBody);
-
-            if (response.IsSuccessStatusCode)
-            {
-                RootObject result = JsonConvert.DeserializeObject<RootObject>(responseBody);
-                return result;
-            }
-            else
-            {
-                throw new Exception($"Request failed with status code {response.StatusCode}. Error message: {responseBody}");
-            }
-        }
-    }
-
     public class Program
     {
         public static async Task Main(string[] args)
         {
-            네이버좌표변환ApiClient geocodeApiClient = new("qnmcxa3j8i", "onuvNfUFBqgm0QGkXHjTWPk7HGoW8BYTU3qipppP");
+            네이버좌표변환ApiClient geocodeApiClient = new("qnmcxa3j8i", "VyFooPIUNRzFrcm0eFwXzX6Kdlf5V9SoDoYUVz8e");
 
-            var response = await geocodeApiClient.GetGeocodeAsync("인천광역시 강화군 내가면 해안서로 850 경인북부수협 판매사업팀\r\n ");
-            Console.WriteLine("Status: " + response.status);
-            Console.WriteLine("Meta Total Count: " + response.meta.totalCount);
-            Console.WriteLine("Road x: " + response.addresses[0].x);
-            Console.WriteLine("Road x: " + response.addresses[0].y);
+            var response1 = await geocodeApiClient.GetGeocodeAsync("인천광역시 강화군 내가면 해안서로 850 경인북부수협 판매사업팀");
+            Console.WriteLine("Status: " + response1.status);
+            Console.WriteLine("Meta Total Count: " + response1.meta.totalCount);
+            Console.WriteLine("Road x: " + response1.addresses[0].x);
+            Console.WriteLine("Road x: " + response1.addresses[0].y);
+
+            var response2 = await geocodeApiClient.GetGeocodeAsync("인천광역시 옹진군 영흥면 외리 247-53 영흥수협김건조장");
+            Console.WriteLine("Status: " + response2.status);
+            Console.WriteLine("Meta Total Count: " + response1.meta.totalCount);
+            Console.WriteLine("Road x: " + response2.addresses[0].x);
+            Console.WriteLine("Road x: " + response2.addresses[0].y);
+
+            네이버길찾기ApiClient directionApiClient = new("qnmcxa3j8i", "VyFooPIUNRzFrcm0eFwXzX6Kdlf5V9SoDoYUVz8e");
+            await directionApiClient.TestGetDrivingDirectionsAsync($"{response1.addresses[0].x},{response1.addresses[0].y}", $"{response2.addresses[0].x}.{response2.addresses[0].y}");
+            
         }
     }
 }
